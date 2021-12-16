026b303aed22fc5426b7bf4b3813ed57a3b1b2d6
adrien <adrien.peyrache@gmail.com>
2021-12-16 14:12:04 -0500

    Update PCA-based replay

--- replay/pynacollada_replay.py
+++ replay/pynacollada_replay.py
@@ -2,35 +2,47 @@
 """
 Created on Wed Dec 15 19:11:53 2021
 
-@author: apeyra4
-"""
+@author: Adrien Peyrache
 
+This script shows how to use pynapple to compute sleep reactivation, step by step.
+See pynacollada_replayExample for a real case example
+
+"""
 
 import pynapple as nap
 import pandas as pd
 import numpy as np
 import seaborn as sns; sns.set_theme()
+import matplotlib.pyplot as plt
+
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 from scipy import stats
 
-sessionEpoch = nap.IntervalSet(start = 0, end = 3000, time_units = 's')
-wakeEpoch = nap.IntervalSet(start = 1000, end = 1999, time_units = 's')
-sleepPre = nap.IntervalSet(start = 0, end = 999, time_units = 's')
-sleepPost = nap.IntervalSet(start = 2000, end = 3000, time_units = 's')
 
+recDuration = 2000.; #recording duration in secs
+sessionEpoch = nap.IntervalSet(start = 0, end = recDuration, time_units = 's')
+wakeEpoch = nap.IntervalSet(start = 0, end = 999, time_units = 's')
+sleepEp = nap.IntervalSet(start = 1000, end = recDuration, time_units = 's')
+
+# Let's imagine we have done some sleep scoring identifying NREM sleep episodes
+nremEp = nap.IntervalSet(start = [1100, 1500], end = [1200, 1700], time_units = 's')
+
+# We just create fake spike trains
 spikes =  {}
 
 for s in range(50):
     random_rate = 10 * np.random.rand(1)
-    random_times = np.random.uniform(0, 3000, int(np.rint(3000.0 * random_rate)))
+    random_times = np.random.uniform(0, recDuration, int(np.rint(recDuration * random_rate)))
     random_times = np.sort(random_times)
     my_spike = nap.Ts(random_times, time_units = 's')
     spikes[s] = my_spike
     
+#Now we define pynapple's TsGroup (perfectly suited for spike trains)    
 spikeGrp = nap.TsGroup(data = spikes, time_support = sessionEpoch)
 
+#We can immediately bin the spike trains (here in 100ms bins)
 binnedSpk = spikeGrp.count(0.1)
 
 binWake = binnedSpk.restrict(wakeEpoch).values
@@ -50,46 +62,42 @@ pca.fit(zSpkWake)
 sns.lineplot(data=pca.explained_variance_)
 
 #And the first three eigenvectors (the PCs)
+plt.figure()
 ax = sns.heatmap(pca.components_[:3,:])
 ax.set_xlabel("Neurons", fontsize = 15)
 ax.set_ylabel("PCs", fontsize = 15)
 
 # Conpute the z-scored binned spike trains durint sleep
-binPre = binnedSpk.restrict(sleepPre).values
-binPost = binnedSpk.restrict(sleepPost).values
-zSpkPre = StandardScaler().fit_transform(binPre)
-zSpkPost = StandardScaler().fit_transform(binPost)
+binSleep = binnedSpk.restrict(sleepEp).values
+zSpkSleep = StandardScaler().fit_transform(binSleep)
 
 # We project onto the first three PCs
 
-reactPCA = np.zeros((zSpkPre.shape[0],3))
+reactPCA = np.zeros((zSpkSleep.shape[0],3))
     
 for n in range(3):
     pc = pca.components_[n,:]
-    proj = np.dot(zSpkPre,pc)
+    proj = np.dot(zSpkSleep,pc)
     #yes, some maths tricks here
-    diagTerm = zSpkPre*np.tile(pc,(zSpkPre.shape[0],1))
+    diagTerm = zSpkSleep*np.tile(pc,(zSpkSleep.shape[0],1))
     tmp = np.square(proj) - np.sum(np.square(diagTerm),axis=1)
     reactPCA[:,n] = np.transpose(tmp);
 
 # Here, it is time to transform the data back into tsdFrame.
-reactPCA = pd.DataFrame(data=reactPCA, index=binnedSpk.restrict(sleepPre).index)
-
-####### THIS WORKS BUT HOW TO DISPLAY TIME IN SEC??
-x = sns.lineplot(data=reactPCA)
-ax.set_ylabel("Reactivation Strength", fontsize = 15)
-
-
+reactPCA = pd.DataFrame(data=reactPCA, index=binnedSpk.restrict(sleepEp).times(), columns=['PC1','PC2','PC3'])
+# and to pynapple (TsdFrame object) so that time units is a no brainer
 reactPCA = nap.TsdFrame(reactPCA)
 
-####### THIS DOES NOT WORKS, WHY???
-ax = sns.lineplot(data=reactPCA.as_dataframe)
+
+# Plot reactivation strength during a subset of sleepPre
+exampleEp =  nap.IntervalSet(start = [1100,1300], end = [1200,1350], time_units = 's')
+plt.figure()
+ax = sns.lineplot(data=reactPCA.restrict(exampleEp).as_units('s'))
 ax.set_ylabel("Reactivation Strength", fontsize = 15)
 
-
-
-
-
-
-
+# Plot reactivation strength during NREM. We intersect the two intervalSet
+exampleEp =  sleepEp.intersect(nremEp)
+plt.figure()
+ax = sns.lineplot(data=reactPCA.restrict(exampleEp).as_units('s'))
+ax.set_ylabel("Reactivation Strength", fontsize = 15)
 
